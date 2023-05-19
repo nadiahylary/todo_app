03ffcdd036c22982ba0864c7a64d8907c8de6491
Nadia HYLARY <nhylary12@gmail.com>
2023-05-20 00:32:15 +0100

    project setup

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:todo_app/ui_updates_demo.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: Scaffold(
-      appBar: AppBar(
-        title: const Text('Flutter Internals'),
-      ),
-      body: const UIUpdatesDemo(),
-    ),
-  ));
+  runApp(const MyApp());
 }
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter Internals'),
+        ),
+        body: const UIUpdatesDemo(),
+      ),
+    );
+  }
+}
+
